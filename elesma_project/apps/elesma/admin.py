ee1c0f433a5d2d9f23a228bd64661110511309dc
Luke Hatcher <lukeman@gmail.com>
2010-08-14 13:51:20 -0400

    Added inlines to the Recipe admin.

--- elesma_project/apps/elesma/admin.py
+++ elesma_project/apps/elesma/admin.py
@@ -1,8 +1,14 @@
 from models import Recipe, Ingredient, Category, Container, RecipeItem
 from django.contrib import admin
 
-admin.site.register(Recipe)
+class RecipeItemInline(admin.TabularInline):
+    model = RecipeItem
+    extra = 4
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (RecipeItemInline,)
+
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient)
 admin.site.register(Category)
 admin.site.register(Container)
-admin.site.register(RecipeItem)
