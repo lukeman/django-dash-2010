0f1072a5cd5e624b6d5dbb23cd6adcaf411d8dfe
Luke Hatcher <lukeman@gmail.com>
2010-08-14 13:44:26 -0400

    Fixed post login url.

--- elesma_project/settings.py
+++ elesma_project/settings.py
@@ -178,7 +178,7 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGIN_URL = "/account/login/" # @@@ any way this can be a url name?
-LOGIN_REDIRECT_URLNAME = "what_next"
+LOGIN_REDIRECT_URLNAME = "home"
 
 EMAIL_CONFIRMATION_DAYS = 2
 EMAIL_DEBUG = DEBUG
