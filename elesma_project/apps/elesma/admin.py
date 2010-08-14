c727ba73c226cf807a402c37cd534bd34cc67743
lukeman <lukeman@gmail.com>
2010-08-14 03:32:14 -0400

    Hooked up the admin. Now is the part of the show where we danc^H^H^H^Hsleep.

---
+++ elesma_project/apps/elesma/admin.py
@@ -0,0 +1,8 @@
+from models import Recipe, Ingredient, Category, Container, RecipeItem
+from django.contrib import admin
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Category)
+admin.site.register(Container)
+admin.site.register(RecipeItem)
