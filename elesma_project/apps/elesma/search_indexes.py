bf60a6c8cbcffbe7cdadbc947777d1302e423964
Will Larson <lethain@gmail.com>
2010-08-14 17:32:42 -0700

    Added search index for ingredients to improve quality of search suggestions

--- elesma_project/apps/elesma/search_indexes.py
+++ elesma_project/apps/elesma/search_indexes.py
@@ -1,7 +1,7 @@
 import datetime
 from haystack.indexes import *
 from haystack import site
-from elesma.models import Recipe
+from elesma.models import Recipe, Ingredient
 import settings
 
 SEARCH_INDEX_CLASS = (settings.DEBUG and RealTimeSearchIndex) or SearchIndex
@@ -15,5 +15,13 @@ class RecipeIndex(SEARCH_INDEX_CLASS):
         """Used when the entire index for model is updated."""
         return Recipe.objects.all()
 
+class IngredientIndex(SEARCH_INDEX_CLASS):
+    text = CharField(document=True, use_template=True)
+    name = CharField(model_attr='name')
+
+    def get_queryset(self):
+        """Used when the entire index for model is updated."""
+        return Ingredient.objects.all()
 
 site.register(Recipe, RecipeIndex)
+site.register(Ingredient, IngredientIndex)
